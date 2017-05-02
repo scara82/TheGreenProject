6d93455fce8f1c12b869823ff72ad3408590561f
Marta Scaramella <marta.scaramella@gmail.com>
2017-05-02 10:14:32 +1000

    log out working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user  = User.find_by(email: params[:email])
       if user && user.authenticate(params[:password])
+        user.score = "0"
         session[:id] = user.id
         redirect_to '/home'
       else
