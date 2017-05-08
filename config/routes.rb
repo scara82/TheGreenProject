cb874200277def4aaf6850cdc12c76dfa915ac69
Marta Scaramella <marta.scaramella@gmail.com>
2017-05-08 18:36:27 +1000

    sign in

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,6 @@ Rails.application.routes.draw do
 
   root 'sessions#new'
 
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
